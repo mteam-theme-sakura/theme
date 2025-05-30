b4025e902f5c2140917d675d48e126ef68428606
悦咚 / Dark495 <flandrestudio.cn@gmail.com>
2025-05-30 15:29:40 +0800

    rename

--- dist/mteam-theme.user.js
+++ dist/mteam-theme.user.js
@@ -4,7 +4,7 @@
 // @description 非常高贵的 —— 又高又贵的 M-Team 主题，你问我为什么高贵？你猜猜？
 // @license     WTFPL
 // @supportURL  https://github.com/mteam-theme-sakura/theme/issues
-// @version     2025530.150756
+// @version     2025530.152839
 // @author      Dark495
 // @icon        https://m-team.syzx.me/assets/img/wtf.png
 // @match       https://kp.m-team.cc/*
